0e4d71ea83732bd85e106f0de287867e542c167f
Lorenzo Sicilia <ls@fractalgarden.com>
2014-08-13 22:47:59 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = '92363b66c1a3f704ebfbbb6f37bde81f04f095387404516f6cbadcc545f09f69f4cc41118924856d5c97e2712d30528db8c5020403b8204fda8783231c283010'
